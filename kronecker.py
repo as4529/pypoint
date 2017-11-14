fba85074cb89d7a830bbcb1b55a0d36a04636488
Anuj Sharma <anuj.sharma@columbia.edu>
2017-11-13 22:49:25 -0500

    put kronecker utils in kronecker.py file

--- kronecker.py
+++ kronecker.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import numpy as np
 import optimizers
-from kronecker_utils import kron_mvp, kron_list
 import sys
 import tensorflow.contrib.eager as tfe
 tfe.enable_eager_execution()
@@ -11,7 +10,6 @@ Class for Kronecker inference of GPs
 """
 
 class KroneckerSolver:
-
 
 
     def __init__(self, kernel, likelihood, X, y, tau):
@@ -174,3 +172,62 @@ class KroneckerSolver:
 
         """
         return 0
+
+
+def kron(A, B):
+    """
+    Kronecker product of two matrices
+    TODO: implement this in tensorflow
+    Args:
+        A (tf.Variable): first matrix for kronecker product
+        B (tf.Variable): second matrix
+
+    Returns: kronecker product of A and B
+
+    """
+    return tf.py_func(np.kron, [A, B], tf.float32)
+
+
+def kron_list(matrices):
+    """
+    Kronecker product of a list of matrices
+    Args:
+        matrices (list of tf.Variable): list of matrices
+
+    Returns:
+
+    """
+    out = kron(matrices[0], matrices[1])
+
+    for i in range(2, len(matrices)):
+        out = kron(out, matrices[i])
+
+    return out
+
+def kron_mvp(Ks, v):
+    """
+    Matrix vector product using Kronecker structure
+    Args:
+        Ks (list of tf.Variable): list of matrices corresponding to kronecker decomposition
+        of K
+        v (tf.Variable): vector to multiply K by
+
+    Returns: matrix vector product of K and v
+
+    """
+    if len(Ks) == 1:
+        return tf.matmul(Ks[0], tf.expand_dims(v, 1))
+
+    k_0 = Ks[0]
+    V_rows = k_0.shape[0]
+    V_cols = v.shape[0] / V_rows
+    V = tf.transpose(tf.reshape(v, (V_rows, V_cols)))
+
+    prod = tf.matmul(V, tf.transpose(k_0))
+    mvp = tf.zeros(shape=[0, 1])
+
+    for col in range(prod.shape[1]):
+        mvp = tf.concat([mvp, kron_mvp(Ks[1:], prod[:, col])], 0)
+
+    return mvp
+
