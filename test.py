80e502acb17932123dc31c6e2ea72f03acba4160
Hrishikesh Vaidya <hrishikeshv95@gmail.com>
2017-11-28 01:44:03 -0500

    Test for events sampler for 2D data

--- test.py
+++ test.py
@@ -3,12 +3,17 @@ import GPy
 import edward as ed
 from kern import RBF
 import tensorflow as tf
+import tensorflow.contrib.eager as tfe
+tfe.enable_eager_execution()
 from datasets import build_toy_dataset1, build_toy_dataset2
 from matplotlib import pyplot as plt
-from thinnedEvents import ThinnedEventsSampler, ThinnedEventsSolver
+from thinnedEvents_eager import ThinnedEventsSampler, ThinnedEventsSolver
 
 events, Z, N, rate, measure = build_toy_dataset1()
 kern = RBF(input_dim = 1, lengthscale = 10)
-
-solver = ThinnedEventsSolver(events, kern, measure, rate)
-solver.solve(10)
+sampler = ThinnedEventsSampler(kern, rate=2)
+x_K, y_K, x_M, y_M = sampler.run()
+plt.scatter(x_K[:,0], x_K[:,1])
+plt.show()
+#solver = ThinnedEventsSolver(events, kern, measure, rate)
+#solver.solve(10)
